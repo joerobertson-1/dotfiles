e1a0647f9e1bb974912803ea6cfb0378cf6cd914
joepapier <joe.robertson@hey.com>
2023-06-17 16:38:34 +0100

    Add harpoon, treesitter and vim-fugitive for git

--- config/nvim/lua/joe/packer.lua
+++ config/nvim/lua/joe/packer.lua
@@ -11,5 +11,11 @@ return require('packer').startup(function(use)
 
   use 'folke/tokyonight.nvim'
 
+  use( 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
+
+  use 'theprimeagen/harpoon'
+
+  use 'tpope/vim-fugitive'
+
 end)
 
